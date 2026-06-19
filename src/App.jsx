57a36e1c811df4d3c5b64e686b23c28d038b43e7
Tony Omondi <tonymboya13@gmail.com>
2026-06-20 00:32:51 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-sm">
+        Wave Gliders
+      </h1>
+    </div>
+  )
+}
+
+export default App
