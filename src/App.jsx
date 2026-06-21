60b1f8f4d1662b5abd3d05731c584f9b63a7b5e4
Tony Omondi <tonymboya13@gmail.com>
2026-06-21 20:11:20 +0300

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+    <div className="flex-col max-h-screen items-center justify-center bg-slate-100">
+      <NavBar />
       <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-sm">
         Wave Gliders
       </h1>
