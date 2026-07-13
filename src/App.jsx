3d1088938180d1a073c9fee8abd598657c6ab8bd
Antony Omondi <tonymboya13@gmail.com>
2026-07-13 23:51:04 +0300

    added HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import HomePage from './components/HomePage'
 
 function App() {
   return (
     <div className="min-h-screen bg-[url('/src/assets/wgsc-bg.jpg')] bg-cover bg-center bg-no-repeat">
       <NavBar />
-      <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-sm">
-        Wave Gliders
-      </h1>
+      <HomePage />
     </div>
   )
 }
