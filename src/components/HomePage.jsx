6bddad4895ef34ce7f574215295a9dc350f7d827
Antony Omondi <tonymboya13@gmail.com>
2026-07-14 00:42:46 +0300

    added main div styling

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+    return(
+        <div className="flex justify-center items-center border-1 mt-5 min-h-screen">
+            <h1 className="text-white">Wave Gliders Swim Club</h1>
+        </div>
+    )
+}
