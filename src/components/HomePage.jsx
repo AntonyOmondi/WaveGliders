eddd4cd4fbc38463ebfd340ef15f9803fa05b9b7
Antony Omondi <tonymboya13@gmail.com>
2026-07-29 16:30:05 +0300

    removed blank white space on top of the homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion"
 
 export default function HomePage() {
     return(
-        <div className="flex h-screen flex-col justify-between overflow-hidden mx-5 py-8 px-6 mt-5">
+        <div className="flex h-screen flex-col justify-between overflow-hidden mx-5 py-8 px-6">
             <motion.h1
             initial={{ x: -100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
