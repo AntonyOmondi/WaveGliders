6781d04c6b1d31998d8cbd4c6482b20d50d5b1ac
Antony Omondi <tonymboya13@gmail.com>
2026-07-31 18:17:07 +0300

    added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="min-h-screen bg-[url('/src/assets/wgsc-bg.jpg')] bg-cover bg-center bg-no-repeat">
       <NavBar />
       <HomePage />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer (){
+    return(
+        <div className="border-1 border-white">
+            <a href="">Tel: </a>
+        </div>
+    )
+}
