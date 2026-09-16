8cfae707c6aa5682cadc1bafd1b09d802e6f54a6
Antony Omondi <tonymboya13@gmail.com>
2026-09-16 21:57:17 +0300

    background edited

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
-    <div className="min-h-screen bg-[url('/src/assets/wgsc-bg.jpg')] bg-cover bg-center bg-no-repeat">
-      <NavBar />
-      <HomePage />
-    </div>
+      <div className="min-h-screen bg-[url('/src/assets/wgsc-bg.jpg')] bg-cover bg-center bg-no-repeat">
+        <NavBar />
+        <HomePage />
+      </div>
       <section className='bg-zinc-950/80'>
         <Footer />
       </section>
