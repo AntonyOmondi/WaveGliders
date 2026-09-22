9c6088a735d7d4e10a72422a92562fb4c807ae6b
Antony Omondi <tonymboya13@gmail.com>
2026-09-22 19:05:43 +0300

    added section id for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import HomePage from './components/HomePage'
 import Footer from './components/Footer'
 import Programmes from './components/Programmes'
 import Services from './components/Services'
+import AboutUs from './components/AboutUs'
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-[url('/src/assets/wgsc-bg.jpg')] bg-cover bg-center bg-no-repeat">
         <NavBar />
-        <HomePage />
+        <section id='home'>
+          <HomePage />
+        </section>
       </div>
       <Programmes />
-      <Services />
+      <section id='service'>
+        <Services />
+      </section>   
+      <section id='about'>
+        <AboutUs />
+      </section>
       <section className='bg-zinc-950/80'>
         <Footer />
       </section>
